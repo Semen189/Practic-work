b2404be0e8eddb3d3000d9fd31e85ac6da623bc4
Семен <semenromashov@mail.ru>
2020-06-19 14:32:30 +0300

    first commit

---
+++ Desktop/Excel/Excel/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+using System.Windows;
+
+namespace Excel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _Application excel = new _Excel.Application();
+            Workbook wb;
+            Worksheet ws;
+            int i = 1, j = 1;
+            wb = excel.Workbooks.Open(@"C:\Users\Семен\Desktop\Test");
+            ws = wb.Worksheets[1];
+            if (ws.Cells[i, j].Value2 != null)
+               MessageBox.Show(ws.Cells[i, j].Value2);
+            else
+              MessageBox.Show("");
+
+        }
+    }
+}
